8e84be649d86cda1e7ee510c3084719e1b8246cd
vleadadmin <admin@vlabs.ac.in>
2024-11-25 17:23:50 +0000

    https://virtual-labs.github.io/exp-effect-position-surface-iitb click on the link to test your code.

---
+++ simulation/js/data.js
@@ -0,0 +1,32 @@
+let D = 0.25;
+let Tf = 20;
+let Ts = 120;
+let K = 0.03;
+let Pr = 0.697;
+let nu = 2.076 * Math.pow(10, -6);
+let g = 9.81;
+let Tm;
+let beta;
+let del_T;
+let A;
+// ====== Case 1 ===== //
+let Nu_1;
+let L_1;
+let Gr_1;
+let h_1;
+let Q_1;
+// ====== Case 2 ===== //
+let Nu_2;
+let h_2;
+let Q_2;
+// ====== Case 3 ===== //
+let Nu_3;
+let L_3;
+let Gr_3;
+let h_3;
+let Q_3;
+let inc;
+let Q_a2 = [];
+let r2_a2 = [];
+let r_c1;
+//# sourceMappingURL=data.js.map
